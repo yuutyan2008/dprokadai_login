11bac9711d3cf84dec834115cb0fea3197281578
takanosatomi <ricebread778899@gmail.com>
2024-09-30 23:53:56 +0900

    userのvalidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: { message: "省略できません" }, length: { maximum: 30 }#presence: true⇛name属性は必ず入力
+  validates :name, presence: true, length: { maximum: 30 }#presence: true⇛name属性は必ず入力
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   # 大文字と小文字の区別をなくす
   before_validation { email.downcase! }
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   # has_secure_passwordと書くことでユーザーが新規登録を行う際には password と password_confirmation が必須になります
   has_secure_password
 
-    # 省パスワードにバリデーションを設定
+    # パスワードにバリデーションを設定
     validates :password, length: { minimum: 6 }, allow_nil: true 
 
     # User モデルと Task モデルの間にアソシエーションを設定
